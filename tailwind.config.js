a8758d7a9970868feceb03bd65c13ef8825a81ff
karthik nayak <karunayak63@gmail.com>
2022-01-13 23:03:36 +0530

    feat: add YouTube color schemes.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,20 @@ module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
     extend: {
+      colors: {
+        pdark: "#181818",
+        sdark: "#212121",
+        tdark: "#3d3d3d",
+        light: "#aaaaaa",
+        yred: "#FF0000",
+      },
       fontSize: {
         xxs: "0.65rem",
       },
+      spacing: {
+        18: "4.5rem",
+      },
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/line-clamp")],
 };
