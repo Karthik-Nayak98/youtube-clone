e38d05856ecefa6b22404f4c2baa276660ed9a52
karthik nayak <karunayak63@gmail.com>
2022-01-15 20:43:28 +0530

    chore: add search and comment api url

--- src/constants/apiUrl.js
+++ src/constants/apiUrl.js
@@ -1,12 +1,15 @@
-// const VIDEOS_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&maxResutls=20&chart=mostPopular&key=${process.env.REACT_APP_YOUTUBE_API}`;
 export const BASE_URL = `https://youtube.googleapis.com/youtube/v3`;
 const VIDEOS_API = `${BASE_URL}/videos`;
 const SUBSCRIPTION_API = `${BASE_URL}/subscriptions`;
 const CHANNEL_API = `${BASE_URL}/channels`;
-
-// const VIDEO_DETAILS = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=jR4AG5LdKYE&key=${process.env.REACT_APP_YOUTUBE_API}`
-
-// export const CHANNEL_API = `https://youtube.googleapis.com/youtube/v3/channels?part=snippet&id=${channelId}&key=${process.env.REACT_APP_YOUTUBE_API}`;
+const SEARCH_API = `${BASE_URL}/search`;
+const COMMENT_THREAD_API = `${BASE_URL}/commentThreads`;
 
 export default BASE_URL;
-export { VIDEOS_API, SUBSCRIPTION_API, CHANNEL_API };
+export {
+  VIDEOS_API,
+  SUBSCRIPTION_API,
+  SEARCH_API,
+  CHANNEL_API,
+  COMMENT_THREAD_API,
+};
