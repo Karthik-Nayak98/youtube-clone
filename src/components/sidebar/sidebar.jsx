6140492a419c09d65f435a9d39d9eb722832030e
karthik nayak <karunayak63@gmail.com>
2022-01-15 20:48:02 +0530

    feat: make sidebar stick to the left of the screen

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -1,35 +1,33 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import { CgPlayList } from 'react-icons/cg';
-import { MdHome, MdSubscriptions } from 'react-icons/md';
-import { AiFillLike } from 'react-icons/ai';
+import { CgPlayList } from "react-icons/cg";
+import { MdHome, MdSubscriptions } from "react-icons/md";
+import { AiFillLike } from "react-icons/ai";
 
 function SideBar() {
   return (
-    <div className="h-screen bg-gray-900 text-white">
-      <ul className="pt-4 px-1 flex flex-col justify-center gap-5">
-        <Link to="/" className="flex flex-col justify-center items-center">
-          <MdHome className="text-2xl" />
-          <p className="text-xxs">Home</p>
+    <div className='fixed top-14 h-screen bg-sdark text-white w-18'>
+      <ul className='pt-4 px-1 flex flex-col justify-center gap-8'>
+        <Link to='/' className='flex flex-col justify-center items-center'>
+          <MdHome className='text-2xl' />
+          <p className='text-xxs'>Home</p>
         </Link>
         <Link
-          to="/playlist"
-          className="flex flex-col justify-center items-center"
-        >
-          <CgPlayList className="text-3xl" />
-          <p className="text-xxs">Playlist</p>
+          to='/playlist'
+          className='flex flex-col justify-center items-center'>
+          <CgPlayList className='text-3xl' />
+          <p className='text-xxs'>Playlist</p>
         </Link>
         <Link
-          to="/subscribe"
-          className="flex flex-col justify-center items-center"
-        >
-          <MdSubscriptions className="text-xl" />
-          <p className="text-xxs">Subscription</p>
+          to='/subscribe'
+          className='flex flex-col justify-center items-center'>
+          <MdSubscriptions className='text-xl' />
+          <p className='text-xxs'>Subscription</p>
         </Link>
-        <Link to="/liked" className="flex flex-col justify-center items-center">
-          <AiFillLike className="text-2xl" />
-          <p className="text-xxs">Liked Videos</p>
+        <Link to='/liked' className='flex flex-col justify-center items-center'>
+          <AiFillLike className='text-2xl' />
+          <p className='text-xxs'>Liked Videos</p>
         </Link>
       </ul>
     </div>
@@ -37,5 +35,3 @@ function SideBar() {
 }
 
 export default SideBar;
-
-<div> Home</div>;
