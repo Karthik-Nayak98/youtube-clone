4f442a01f45d0afc04952c567778d6f32c01f20e
karthik nayak <karunayak63@gmail.com>
2022-01-15 20:54:02 +0530

    feat: add routing functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,21 @@
+import { Routes, Route } from "react-router-dom";
+
+import VideoPlayer from "./videoPlayer/videoplayer";
+import NavBar from "./navbar/navbar";
+import SideBar from "./sidebar/sidebar";
+import VideoList from "./videolist/videolist";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className='App'>
+      <NavBar />
+      <SideBar />
+      <Routes>
+        <Route path='/' element={<VideoList />} />
+        <Route path=':videoId' element={<VideoPlayer />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
