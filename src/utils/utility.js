be59daa2ad109678949df0d7fe35778f76551b9a
karthik nayak <karunayak63@gmail.com>
2022-01-21 21:22:51 +0530

    fix: update format for displaying duration.

--- src/utils/utility.js
+++ src/utils/utility.js
@@ -1,12 +1,16 @@
 export const durationFormatter = (duration = "PTM0S0") => {
   const array = duration.match(/\d+/g);
-  if (array.length === 3) if (array[2].length === 1) array[2] = `0${array[2]}`;
-
+  if (array.length === 3) {
+    if (array[1].length === 1) array[1] = `0${array[1]}`;
+    if (array[2].length === 1) array[2] = `0${array[2]}`;
+  }
   if (array.length === 2) if (array[1].length === 1) array[1] = `0${array[1]}`;
 
   if (array.length === 1)
     if (array[0].length === 1 && duration.indexOf("M") > -1)
       array[0] = `${array[0]}:00`;
+    else if (array[0].length === 1 && duration.indexOf("H") > -1)
+      array[0] = `${array[0]}:00:00`;
     else array[0] = `0:${array[0]}`;
 
   return array.join(":");
