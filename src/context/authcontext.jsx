03b037ea862cff498f84d7a3a804d8a45911f50d
karthik nayak <karunayak63@gmail.com>
2022-01-21 22:12:31 +0530

    fix: import useEffect from correct library

--- src/context/authcontext.jsx
+++ src/context/authcontext.jsx
@@ -1,5 +1,4 @@
-import React, { createContext, useState } from "react";
-import { useEffect } from "react/cjs/react.development";
+import React, { useEffect, createContext, useState } from "react";
 
 export const AuthContext = createContext();
 
